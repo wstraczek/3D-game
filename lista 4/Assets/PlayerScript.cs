c675c9382fdc7803c87857115c064abbdbf5c847
Weronika Strączek <w.straczek@campus.fct.unl.pt>
2021-06-02 17:59:57 +0200

    game commit

---
+++ lista 4/Assets/PlayerScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    private int health;
+    public Text healthText;
+    public Text ballsText;
+    private int balls = 5;
+    public AudioSource collect;
+    void Start()
+    {
+        health = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShowHealth();
+        ShowBalls();
+    }
+
+    void OnCollisionEnter(Collision coll) {
+        Debug.Log("Collision");
+
+        if (coll.gameObject.tag == "Enemy") {
+            health--;
+        }
+
+        if (health == 0) {
+            SceneManager.LoadScene("GameScene");
+        }
+
+        if (coll.gameObject.tag == "Ball") {
+            collect.Play();
+            balls--;
+            Destroy(coll.gameObject);
+        }
+
+        if (balls == 0) {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+
+    public void ShowHealth() {
+        string message = health.ToString();
+        healthText.text = message;
+    }
+
+    public void ShowBalls() {
+        string message = balls.ToString();
+        ballsText.text = message;
+    }
+}
---
+++ lista 4/Assets/RifleScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RifleScript : MonoBehaviour {
+    private float damage = 30f;
+    private float range = 100f;
+    private int enemies = 5;
+
+    public ParticleSystem muzzleFlash;
+    public AudioSource shootSound;
+    public AudioSource dieSound;
+    public Text enemiesText;
+
+    public Camera fpsCam;
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+        ShowEnemies();
+    }
+
+    void Shoot() {
+        muzzleFlash.Play();
+        shootSound.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
+            Debug.Log(hit.transform.name);
+            EnemyScript enemy = hit.transform.GetComponent<EnemyScript>();
+            if (enemy != null) {
+                if (enemy.getHealth() - damage <= 0f) killEnemy();
+                enemy.takedamage(damage);
+            }
+        }
+    }
+
+    public void killEnemy() {
+        enemies--;
+        dieSound.Play();
+    }
+
+    public void ShowEnemies() {
+        string message = enemies.ToString();
+        enemiesText.text = message;
+    }
+
+    
+
+}
+
